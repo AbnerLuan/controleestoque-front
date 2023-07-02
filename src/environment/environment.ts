cd3a8c9d4fdca3c32063b7c33f44986bc914e273
Abner Luan <abnerldsantosgmail.com>
2023-07-02 17:40:02 -0300

    alteracao environment

---
+++ src/environment/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+//    url: 'http://localhost:8080/api/',
+//    urlLogin: 'http://localhost:8080/'
+
+        url: 'https://controleestoque-production-f8b5.up.railway.app/api/',
+   urlLogin: 'https://controleestoque-production-f8b5.up.railway.app/'
+}
